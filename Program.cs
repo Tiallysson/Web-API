f3251817711d51300847e85b9d91773a1de7fdef
Tiallysson Costa <tiallysson@proton.me>
2024-07-29 21:01:23 -0300

    Adicionei os arquivos principais da API

--- Program.cs
+++ Program.cs
@@ -1,32 +1,30 @@
-using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Users.Data; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adicionar serviços ao contêiner
+// Adicionar serviços ao container
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserAPI", Version = "v1" });
-    c.CustomSchemaIds(type => type.FullName);
-});
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Adicionar outros serviços como Swagger, etc.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configurar o pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PetHealth V1");
-});
-
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
 
-app.MapControllers(); // Mapear controladores para endpoints
+app.MapControllers();
 
 app.Run();
